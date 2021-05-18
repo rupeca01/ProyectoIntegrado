7b20b13571089d5642e6fc2c03f982f0ffb35059
ivanlop8 <73492315+ivanlop8@users.noreply.github.com>
2021-05-18 18:04:05 +0200

    Datos BBDD

---
+++ datos.sql
@@ -0,0 +1,8 @@
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (165, 100, 'Chicken fillet', 'Protein');
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (112, 100, 'Turkey fillet', 'Protein'); 
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (72, 100, 'Hake', 'Protein');
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (129, 100, 'Rice', 'carbohydrates');
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (157, 100, 'Macaroni', 'carbohydrates');
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (378, 100, 'Cereals Corn Flakes', 'carbohydrates');
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (884, 100, 'Olive Oil', 'Fats');
+INSERT INTO alimentos (calorias, cantidad, nombre, tipo_Alimentos) VALUES (590, 100, 'Almonds', 'Fats');
