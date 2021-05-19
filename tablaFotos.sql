7ca64b0f7278b9281448d18ca171127004980255
ivanlop8 <73492315+ivanlop8@users.noreply.github.com>
2021-05-19 11:37:06 +0200

    Delete tablaFotos.sql

--- tablaFotos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE fotos (
-	id INT NOT NULL AUTO_INCREMENT,
-	nombre VARCHAR(45) NOT NULL,
-	imagen LONGBLOB NOT NULL,
-	idUsuario INT(9),
-	FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
-	PRIMARY KEY (id));
